ca79ed2a64d85daa99a205d8dfbd8acd72f74c25
Jhoel <jhoel.sv25@gmail.com>
2025-08-28 22:23:13 -0500

    refactor: improve permissions guard logic to handle multiple required permissions

--- src/auth/guards/permissions.guard.ts
+++ src/auth/guards/permissions.guard.ts
@@ -7,26 +7,21 @@ export class PermissionsGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredPermission = this.reflector.getAllAndOverride<string[]>(PERMISSION_KEY, [
+    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(PERMISSION_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    if (!requiredPermission) {
+    if (!requiredPermissions) {
       return true; // no requiere permiso, se permite
     }
 
     const { user } = context.switchToHttp().getRequest();
+    const userPermissions = user?.permissions || [];
 
-    if (!user || !user.role || !user.role.permissions) {
-      throw new ForbiddenException('No tienes permisos para esta acción');
-    }
-    const userPermissions = user.permissions || []; // Asegurarse de que siempre sea un array
-
-    if (!userPermissions.includes(requiredPermission)) {
+    if (!requiredPermissions.every(permission => userPermissions.includes(permission))) {
       throw new ForbiddenException('No tienes permiso para esta acción');
     }
-
-    return requiredPermission.every(permission => userPermissions.includes(permission));
+    return true;
   }
 }
