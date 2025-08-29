8d1228cf071d7528af1b8145a5c1d2c793b3d896
Jhoel <jhoel.sv25@gmail.com>
2025-08-28 22:37:06 -0500

    refactor: update CORS configuration to ensure credentials are handled as a boolean

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -7,6 +7,6 @@ export const corsConfig = (configService: ConfigService): CorsOptions => {
     methods: configService.get<string>('CORS_METHODS') || 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders:
       configService.get<string>('CORS_ALLOWED_HEADERS') || 'Content-Type, Accept, Authorization',
-    credentials: configService.get<boolean>('CORS_CREDENTIALS') || true,
+    credentials: String(configService.get<string>('CORS_CREDENTIALS')).toLowerCase() === 'true',
   };
 };
