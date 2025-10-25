7c4f3b2779a0c7f404504b42e321b4fab10f06fb
Jhoel <jhoel.sv25@gmail.com>
2025-10-25 13:08:40 -0500

    feat: enhance CreateSectionDto and SectionEntity with additional properties and enums for status, shift, tutor, classroom, available slots, grade, and academic year

--- src/features/sections/entities/section.entity.ts
+++ src/features/sections/entities/section.entity.ts
@@ -1,35 +1,36 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { ClassroomEntity } from '../../classrooms/entities/classroom.entity';
-import { GradeEntity } from '../../grades/entities/grade.entity';
-
-export enum SectionType {
-  A = 'A',
-  B = 'B',
-  C = 'C',
-  D = 'D',
-}
+import { BaseEntity } from '@common/entities/base.entity';
+import { StatusType } from '@common/enums/global.enum';
+import { AcademicYearEntity } from '@features/academic_years/entities/academic_year.entity';
+import { GradeEntity } from '@features/grades/entities/grade.entity';
+import { SectionShift } from '@features/sections/enums/section.enum';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity('sections')
-export class SectionEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
+export class SectionEntity extends BaseEntity {
   @Column({ type: 'varchar', length: 50 })
   name: string; // A, B, C
 
   @Column({ type: 'int', nullable: true })
   capacity?: number; // Cantidad máxima de estudiantes
 
+  @Column({ type: 'enum', enum: StatusType, default: StatusType.ACTIVE })
+  status: StatusType;
+
+  @Column({ type: 'enum', enum: SectionShift, default: SectionShift.MORNING })
+  shift: SectionShift; //TODO: Relación con el turno (mañana, tarde, noche)
+  @Column({ type: 'varchar', length: 100 })
+  tutor: string; //TODO: Relación con el perfil del tutor
+
+  @Column({ type: 'varchar', length: 100 })
+  classroom: string;
+
+  @Column({ type: 'int' })
+  availableSlots: number; //TODO: Espacios disponibles en la sección
+
   @ManyToOne(() => GradeEntity, grade => grade.sections)
   @JoinColumn({ name: 'gradeId' })
   grade: GradeEntity;
 
-  @Column()
-  gradeId: string;
-
-  @Column({ type: 'boolean', default: true })
-  isActive: boolean;
-
-  @OneToMany(() => ClassroomEntity, classroom => classroom.section, { cascade: true })
-  classrooms: ClassroomEntity[];
+  @ManyToOne(() => AcademicYearEntity)
+  yearAcademic: AcademicYearEntity;
 }
