3a914cf04c1828e93a92c8403a72d1c8b5bfe734
Jhoel <jhoel.sv25@gmail.com>
2025-11-07 23:05:59 -0500

    feat: enhance AssigmentSubmissionsService and AssigmentSubmissionsController with error handling and improved DTO usage

--- src/features/assigment_submissions/assigment_submissions.controller.ts
+++ src/features/assigment_submissions/assigment_submissions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { AssigmentSubmissionsService } from './assigment_submissions.service';
 import { CreateAssigmentSubmissionDto } from './dto/create-assigment_submission.dto';
 import { UpdateAssigmentSubmissionDto } from './dto/update-assigment_submission.dto';
@@ -8,27 +8,27 @@ export class AssigmentSubmissionsController {
   constructor(private readonly assigmentSubmissionsService: AssigmentSubmissionsService) {}
 
   @Post()
-  create(@Body() createAssigmentSubmissionDto: CreateAssigmentSubmissionDto) {
-    return this.assigmentSubmissionsService.create(createAssigmentSubmissionDto);
+  create(@Body() dto: CreateAssigmentSubmissionDto) {
+    return this.assigmentSubmissionsService.create(dto);
   }
 
   @Get()
-  findAll() {
-    return this.assigmentSubmissionsService.findAll();
+  findAll(@Query() query: any) {
+    return this.assigmentSubmissionsService.findAll(query);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.assigmentSubmissionsService.findOne(+id);
+    return this.assigmentSubmissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAssigmentSubmissionDto: UpdateAssigmentSubmissionDto) {
-    return this.assigmentSubmissionsService.update(+id, updateAssigmentSubmissionDto);
+  update(@Param('id') id: string, @Body() dto: UpdateAssigmentSubmissionDto) {
+    return this.assigmentSubmissionsService.update(id, dto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.assigmentSubmissionsService.remove(+id);
+    return this.assigmentSubmissionsService.remove(id);
   }
 }
